909f7cdb5a2f6b767002ad6b887ade0b9ea0a0a7
Devy <izabela.marcinkowska@outlook.com>
2024-02-16 21:59:47 +0100

    style: Refactor button to tailwind styling

--- src/App.tsx
+++ src/App.tsx
@@ -32,15 +32,17 @@ function App() {
 
   return (
     <>
-      <div className="styling-wrapper">
+      <div className="max-w-screen-xl h-screen overflow-auto mx-[auto] my-[0] p-8 text-center">
         <Background />
         <div className="flex flex-col gap-24 mt-44 justify-center">
-          <h1 className="text-6xl z-50 white-shadow">Wanna be my Valentine?</h1>
+          <h1 className="text-2xl md:text-6xl z-50 white-shadow">
+            Wanna be my Valentine?
+          </h1>
           <div className="flex justify-center gap-16 z-50">
             <h1 className="kreep">
               <button
                 onClick={handleYesClick}
-                className="button button5 link buzz-out-on-hover"
+                className="button bg-green-600 text-slate-100 border-2 border-solid border-green-800 xl:p-16 p-5 text-center no-underline inline-block text-xs md:text-2xl mx-0.5 my-1 duration-[400ms] cursor-pointer rounded-[20%] hover:hover:bg-[#1e8b3f] hover:animate-none link buzz-out-on-hover"
               >
                 <b>{positiveTexts[yesText]}</b>
               </button>
