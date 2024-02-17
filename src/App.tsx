a1cdfd51e699f2d8a93f6c3c052557c31e2be7b3
Devy <izabela.marcinkowska@outlook.com>
2024-02-17 17:55:25 +0100

    style: Adjust hover size

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
   const [yesText, setYesText] = useState(0);
 
   const handleNoClick = () => {
-    if (size < 0.05) {
+    if (size < 0) {
       return;
     }
     setSize((prevSize) => Math.round((prevSize - 0.05) * 100) / 100);
@@ -36,7 +36,9 @@ function App() {
       <div className="max-w-screen-xl h-screen overflow-auto mx-[auto] my-[0] p-8 text-center">
         <Background />
         <div className="flex flex-col gap-24 mt-44 justify-center">
-          <h1 className="text-sm md:text-6xl z-50 white-shadow">Wanna be my Valentine?</h1>
+          <h1 className="text-sm md:text-6xl z-50 white-shadow">
+            Wanna be my Valentine?
+          </h1>
           <div className="flex justify-center gap-16 z-50">
             <h1 className="kreep">
               <button
@@ -50,8 +52,8 @@ function App() {
               <button
                 className={`cursor-pointer [transition:all_0.3s_ease-in-out] bg-red-600 text-slate-100 border-2 border-solid border-red-800 rounded-[20%] xl:p-16 p-7 text-center no-underline inline-block text-xs md:text-2xl mx-0.5 my-1`}
                 onClick={handleNoClick}
-                onMouseOver={() => setSize((prevSize) => prevSize - 0.2)}
-                onMouseOut={() => setSize((prevSize) => prevSize + 0.2)}
+                onMouseOver={() => setSize((prevSize) => prevSize - 0.05)}
+                onMouseOut={() => setSize((prevSize) => prevSize + 0.05)}
                 style={{ transform: `scale(${size})` }}
               >
                 {negativeTexts[textIndex]}
