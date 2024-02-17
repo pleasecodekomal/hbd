0072a6c8f3890f2ccb14e486373c11f4a44fbb77
Devy <izabela.marcinkowska@outlook.com>
2024-02-17 15:35:40 +0100

    style: Change padding style sizing from state to transform

--- src/App.tsx
+++ src/App.tsx
@@ -7,16 +7,14 @@ import { negativeTexts, positiveTexts } from './buttonTexts';
 function App() {
   const navigate = useNavigate();
   // No button
-  const [padding, setPadding] = useState(65);
-  const [fontSize, setFontSize] = useState(22);
+  const [size, setSize] = useState(1);
   const [textIndex, setTextIndex] = useState(0);
   // Yes button
   const [yesClicks, setYesClicks] = useState(0);
   const [yesText, setYesText] = useState(0);
 
   const handleNoClick = () => {
-    setPadding(padding - 5);
-    setFontSize(fontSize - 1);
+    setSize((prevSize) => prevSize - 0.05);
     setTextIndex((prevIndex) => (prevIndex + 1) % negativeTexts.length);
   };
 
@@ -35,22 +33,22 @@ function App() {
       <div className="max-w-screen-xl h-screen overflow-auto mx-[auto] my-[0] p-8 text-center">
         <Background />
         <div className="flex flex-col gap-24 mt-44 justify-center">
-          <h1 className="text-2xl md:text-6xl z-50 white-shadow">
+          <h1 className="text-sm md:text-6xl z-50 white-shadow">
             Wanna be my Valentine?
           </h1>
           <div className="flex justify-center gap-16 z-50">
             <h1 className="kreep">
               <button
                 onClick={handleYesClick}
-                className="button bg-green-600 text-slate-100 border-2 border-solid border-green-800 xl:p-16 p-5 text-center no-underline inline-block text-xs md:text-2xl mx-0.5 my-1 duration-[400ms] cursor-pointer rounded-[20%] hover:hover:bg-[#1e8b3f] hover:animate-none link buzz-out-on-hover"
+                className="button bg-green-600 text-slate-100 border-2 border-solid border-green-800 xl:p-16 p-7 text-center no-underline inline-block text-xs md:text-2xl mx-0.5 my-1 duration-[400ms] cursor-pointer rounded-[20%] hover:hover:bg-[#1e8b3f] hover:animate-none link buzz-out-on-hover"
               >
                 <b>{positiveTexts[yesText]}</b>
               </button>
             </h1>
             <div>
               <button
-                className="button-shrink"
-                style={{ padding: `${padding}px`, fontSize: `${fontSize}px` }}
+                className="cursor-pointer [transition:all_0.3s_ease-in-out] bg-red-600 text-slate-100 border-2 border-solid border-red-800 rounded-[20%] xl:p-16 p-7 text-center no-underline inline-block text-xs md:text-2xl mx-0.5 my-1 hover:scale-[0.8]"
+                style={{ transform: `scale(${size})` }}
                 onClick={handleNoClick}
               >
                 {negativeTexts[textIndex]}
